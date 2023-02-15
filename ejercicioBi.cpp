11c4e90894a328e7f4f1d449d37b020ed029e2c0
Alejandro Acedo Ralston <aleaceral@alum.us.es>
2023-02-15 22:14:31 +0100

    commit matrix

--- ejercicioBi.cpp
+++ ejercicioBi.cpp
@@ -9,9 +9,68 @@ const int columnas = 4;
 
 int main(){
 
+int matriz[filas][columnas];
+
+int sumaFilas[filas]={0};
+
+int sumaColumnas[columnas]={0};
+
+cout<<"A continuación, introduzca los valores de la matriz: "<<endl;
+//Introducir valores en la matriz
+for(int i=0;i<filas;i++){
+
+    for(int j=0;j<columnas;j++){
+        cout<<"Introduzca el valor correspondiente a la fila "<<i+1<<" y la columna "<<j+1<<" :"<<endl;
+        cin>>matriz[i][j];
+    }
+}
+
+//Bucle para sumar cada fia
+for(int i=0;i<filas;i++){
+    int sumaFila = 0;
+    for(int j=0;j<columnas;j++){
+
+        sumaFila+=matriz[i][j];
+    }
+    sumaFilas[i]=sumaFila;
+}
+
+//Bucle para sumar cada columna
+for(int j=0;j<columnas;j++){
+    int sumaColumna=0;
+
+    for(int i=0;i<filas;i++){
+
+        sumaColumna+=matriz[i][j];
+    }
+    sumaColumnas[j]=sumaColumna;
+}
+
+//Imprimimos la matriz 
+cout<<"La matriz es: "<<endl;
+for(int i=0;i<filas;i++){
+
+    for(int j=0;j<columnas;j++){
+
+        cout<<matriz[i][j]<<" | ";
+    }
+}
+
+//Imprimimos la suma de cada fila
+
+for(){
 
 
 
+}
+
+//Imprimimos la suma de cada columna
+
+for(){
+
+
+
+}
 
 
 
