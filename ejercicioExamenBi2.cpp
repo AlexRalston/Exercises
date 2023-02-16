6aac65d5774520fe365f430e01174203bd386cd3
Alejandro Acedo Ralston <aleaceral@alum.us.es>
2023-02-16 16:05:27 +0100

    Ejemplo de salida de datos

--- ejercicioExamenBi2.cpp
+++ ejercicioExamenBi2.cpp
@@ -1,3 +1,14 @@
+
+/*Programacion Redes Implantacion  Media
+Alejandro  3.0    4.0           5.0   0.0
+Luis       7.0    3.0           2.0   0.0        
+Juan       2.0    5.0           4.0   0.0
+Antonio    6.0    2.0           8.0   0.0
+Carlos     2.0    1.0           9.0   0.0
+*/
+
+
+
 #include<iostream>
 #include<string.h>
 #include<vector>
