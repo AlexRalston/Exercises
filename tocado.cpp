cfd6117af813c71c89aae729bbdf2b21ef137d0a
Alejandro Acedo Ralston <aleaceral@alum.us.es>
2023-02-23 13:45:59 +0100

    tocado y hundido

--- tocado.cpp
+++ tocado.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 /*Reglas.
 1.No se puede disparar dos veces al mismo sitio.
 2.Para eliminar un barco se necesita acertar en todas sus partes.
@@ -12,25 +13,67 @@ using namespace std;
 
 
 int main(){
+    //Nombre del usuario que va a jugar a hundir la flota
+    string nombre_user = " ";
+    //Opcion a elegir"Si"o "No" para verificar que el nombre del usuario introducir es
+    //correcto.
+    int opcion;
+    
+    cout<<"BIENVENIDO AL JUEGO DE HUNDIR LA FLOTA."<<endl;
+    cout<<"Por favor, introduzca su nombre: "<<endl;
 
+    cin>>nombre_user;
+
+    cout<<"Su nombre es: "<<nombre_user<<endl;
+    cout<<"Es correcto?(1.Si/2.No)."<<endl;
+
+    cin>>opcion;
+
+        if(opcion == 1){
+        cout<<"Prosigamos....";
+        }
+        else if(opcion == 2){
+        main();
+        }
+        else{ 
+
+        cout<<"Opcion invalida.";
+        }
     //El tablero sera de 6x6 en todos los casos
     const int filas=6;
     const int columnas=6;
-    //Declaramos el array correspondiente al tablero
-    tablero[filas][columnas];
 
+
+    //Declaramos el array correspondiente al tablero
+
+    string tablero[filas][columnas]; 
+    //fragata 2 posiciones, acorazado 3 posiciones y el portaviones 4 posiciones.
+    string fragata = "fragata";
+    string acorazado = "acorazado";
+    string portaviones = "portaviones";
+
+    tablero[0][1] = "fragata";
+    tablero[0][2] = "fragata";
+    tablero[1][3] = "acorazado";
+    tablero[1][4] =  "acorazado";
+    tablero[1][5] = "acorazado";
+    tablero[2][1] = "portaviones";
+    tablero[2][2] = "portaviones";
+    tablero[2][3] = " ";
+    tablero[][] 
+
+
+    //Bucle para asignar la fragata, el acorazado y el portaviones
     for(int i=0;i<filas;i++){
         for(int j=0;j<columnas;j++){
-            
 
         }
+
     }
-
-
 
 
     
 
 
-
+return 0;
 }
