090d437a2e20d7bc3540c31d52a068b7d86a5f60
Alejandro Acedo Ralston <aleaceral@alum.us.es>
2023-02-23 20:06:49 +0100

    Temperatura media de cada dia y dias mayores a 30ºC

--- ejercicioExamenBi7.cpp
+++ ejercicioExamenBi7.cpp
@@ -2,6 +2,9 @@
 #include<iostream>
 #include<iomanip>
 
+
+using namespace std;
+
 int main(){
     const int NUM_DIAS=7;
     const int NUM_HORAS=24;
@@ -37,26 +40,28 @@ int main(){
         }
     }
     //Imnprimimos la temperatura maxima de la semana
-    cout<<"Temperatura maxima de la semana: "<<temp_max<<endl;
+    cout<<"Temperatura maxima de la semana: "<<temp_max<<"ºC"<<endl;
     //Imprimimos la temperatura minima de la semana
-    cout<<"Temperatura minima de la semana: "<<temp_min<<endl;
+    cout<<"Temperatura minima de la semana: "<<temp_min<<"ºC"<<endl;
     //Calcular la temperaturas maximas y minimas de cada dia
     for(int i=0;i<NUM_DIAS;i++){
         temp_max = temperaturas[i][0];
         temp_min = temperaturas[i][0];
         for(int j=0;j<NUM_HORAS;j++){
-            if(temperaturas[i][j]> temp_max){
+            if(temperaturas[i][j] > temp_max){
                 temp_max = temperaturas[i][j];
             }
-            if(temperaturas[i][j]<temp_min){
+            if(temperaturas[i][j] < temp_min){
                 temp_min = temperaturas[i][j];
             }
         }
+        cout<<"La temperatura maxima del dia "<<i+1<<" es: "<<temp_max<<"ºC"<<endl;
+        cout<<endl;
+        cout<<"La temperatura minima del dia "<<i+1<<" es: "<<temp_min<<"ºC"<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
     }
-    //Imprimimos la temperatura maxima del dia
-    cout<<"La temperatura maxima del dia "<<i+1<<": "<<temp_max<<endl;
-    //Imprimimos la temperatura minima del dia
-    cout<<"La temperatura minima del dia "<<i+1<<": "<<temp_min<<endl;
     //Calculemos la temperatura media de la semana
     double temp_suma=0;
     for(int i=0;i<NUM_DIAS;i++){
@@ -66,18 +71,39 @@ int main(){
         }
     }
     double temp_media = temp_suma / (NUM_DIAS*NUM_HORAS);
-    cout<<"La temperatura media de la semana es: "<<temp_media<<"."<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"La temperatura media de la semana es: "<<temp_media<<"ºC"<<"."<<endl;
     //Calculemos la temperatura media de cada dia
-    
+    cout<<endl;
+
     for(int i=0;i<NUM_DIAS;i++){
         double media_cada_dia=temperaturas[i][0];
         for(int j=0;j<NUM_HORAS;j++){
-            media_cada_dia +=
+            media_cada_dia += temperaturas[i][j];
             
+
+        }
+        cout<<"La temperatura media del dia "<<i+1<<" es: "<<media_cada_dia<<"ºC"<<endl;
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+    }
+    int num_dias_mayor_treinta=0;
+    for(int i=0;i<NUM_DIAS;i++){
+        for(int j = 0;j<NUM_HORAS;j++){
+            if(temperaturas[i][j] > 30  ){
+                num_dias_mayor_treinta++;
+            }
+             
 
         }
 
     }
+    cout<<endl;
+    cout<<"El numero de dias que superan los 30ºC es "<<num_dias_mayor_treinta<<".";
+
 
 
     return 0;
