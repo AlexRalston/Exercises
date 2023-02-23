3f176cc896ac2a95e73d0e0b0cb6a414882ce8f5
Alejandro Acedo Ralston <aleaceral@alum.us.es>
2023-02-23 01:25:31 +0100

    Ejercicio de examen lista de asignatura y alumnos

--- ejercicioExamenBi2.cpp
+++ ejercicioExamenBi2.cpp
@@ -9,51 +9,72 @@ Carlos     2.0    1.0           9.0   0.0
 
 
 
-#include<iostream>
-#include<string.h>
-#include<vector>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-
-int main(){
-    int alumnos;
-    int asignaturas;
-
-    cout<<"Introduzca el numero de alumnos a evaluar: ";
-    cin>>alumnos;
-    cout<<"Introduzca el numero de asignaturas a evaluar: ";
-    cin>>asignaturas;
-
-    vector<int,string> matrizClase[alumnos][asignaturas];
-
-
-    cout<<"Por favor, introduzca los nombres de los alumnos a evaluar: "<<endl;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<1;j++){
-            cout<<"Introduzca el nombre del alumno "<<i+1<<":"<<endl;
-            
-            cin>>matrizClase[i][0];
-            
-        }
-
+int main() {
+    // Pedir al usuario los nombres de los estudiantes y las asignaturas
+    int num_estudiantes, num_asignaturas;
+    cout << "Introduzca el numero de estudiantes: "<<endl;
+    cin >> num_estudiantes;
+    cout << "Introduzca los nombres de los estudiantes:" << endl;
+    vector<string> nombres_estudiantes(num_estudiantes);
+    for (int i = 0; i < num_estudiantes; i++) {
+        cout << "Estudiante " << i+1 << ": ";
+        cin >> nombres_estudiantes[i];
     }
-    cout<<"Por favor, introduzca los nombres de las asignaturas a evaluar: "<<endl;
-    
-
-    for(int i=0;i<alumnos;i++){
-        for()
-
+    cout << "Introduzca el número de asignaturas: ";
+    cin >> num_asignaturas;
+    cout << "Introduzca los nombres de las asignaturas:" << endl;
+    string nombres_asignaturas[num_asignaturas];
+    for (int i = 0; i < num_asignaturas; i++) {
+        cout << "Asignatura " << i+1 << ": ";
+        cin >> nombres_asignaturas[i];
     }
-    cout<<endl;
-    cout<<endl;
-    cout<<endl;
 
-    for(int i=0;i<alumnos;i++){
-        for(int j=0;j<asignaturas;j++){
-            cout<<matrizClase[i][j]<<" | "<<endl;
+    // Definir el arreglo bidimensional para almacenar las notas de los estudiantes y las asignaturas
+    double notas[num_estudiantes][num_asignaturas];
+
+    // Pedir al usuario las notas de cada estudiante en cada asignatura
+    for (int fila = 0; fila < num_estudiantes; fila++) {
+        cout << "Introduzca las notas de " << nombres_estudiantes[fila] << ":" << endl;
+        for (int columna = 0; columna < num_asignaturas; columna++) {
+            cout << nombres_asignaturas[columna] << ": ";
+            cin >> notas[fila][columna];
         }
     }
 
+    // Imprimir las notas de cada estudiante y la media de cada uno a la derecha de todas sus notas
+    cout << setw(15) << left << "Estudiante";
+    for (int columna = 0; columna < num_asignaturas; columna++) {
+        cout << setw(10) << right << nombres_asignaturas[columna];
+    }
+    cout << setw(15) << right << "Media" << endl;
+    for (int fila = 0; fila < num_estudiantes; fila++) {
+        cout << setw(15) << left << nombres_estudiantes[fila];
+        double suma = 0.0;
+        for (int columna = 0; columna < num_asignaturas; columna++) {
+            cout << setw(10) << right << notas[fila][columna];
+            suma += notas[fila][columna];
+        }
+        double media = suma / num_asignaturas;
+        cout << setw(15) << right << media << endl;
+    }
+
+    // Calcular la media de cada asignatura y mostrarla al final de la tabla
+    cout << setw(15) << left << "Media";
+   for (int columna = 0; columna < num_asignaturas; columna++) {
+    double suma = 0.0;
+    for (int fila = 0; fila < num_estudiantes; fila++) {
+        suma += notas[fila][columna];
+    }
+    double media = suma / num_estudiantes;
+    cout << setw(10) << right << media;
+}
 
 
     return 0;
