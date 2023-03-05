24bd9be3fd0795d23c81182d4545de7d10ef71e8
Alejandro Acedo Ralston <aleaceral@alum.us.es>
2023-03-05 20:38:51 +0100

    tresenraya

--- tresenraya.cpp
+++ tresenraya.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
 #include<stdlib.h>
+
+#include<time.h>//Necesario para generar numeros aleatorios
 using namespace std;
 
 
 
 void loop (char m[3][3]);
 void primera (char m[3][3]);
-void tablero (char m[3][3]);
 
+void tablero (char m[3][3]);
+void Intro_Yo(char m[3][3]);
+void Intro_aleatorio(char m[3][3]);
 int main(){
     char m[3][3];
     loop(m);//Llama a todas las funciones fundamentales y refresh
@@ -16,8 +20,29 @@ int main(){
 }
 
 void loop(char m[3][3]){
+    int i;
+
+    i=0;
+
     primera(m);
+    cout<<endl;
+    cout<<endl;
     tablero(m);
+
+    do{ 
+        system("cls");
+        tablero(m);
+
+        if(i%2==0){
+            Intro_Yo(m);
+        }
+        else{
+            Intro_aleatorio(m);
+        }
+        i++;
+
+    }while (i<=9);
+    
 
 }
 
@@ -50,10 +75,155 @@ void tablero (char m[3][3]){
         if(i<2){
         printf("\n-----------\n");}
     }
-    cout<<endl;
-    cout<<endl;
-    
+}
+
+void Intro_Yo(char m[3][3]){
+    int i, j, k;
+    char aux;
+
+    do{
+        do{
+            cout<<endl;
+            cout<<endl;
+            printf("Coloca una ficha sobre el tablero: ");
+            cout<<endl;
+            cout<<endl;
+
+            fflush(stdin);
+            scanf("%c",&aux);
+
+        }while(aux < '1' || aux > '9');
+    k=0;
+    switch (aux){
+        case '1':{
+            i = 0;
+            j = 0;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre."<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '2':{
+            i = 0;
+            j = 1;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '3':{
+            i = 0;
+            j = 2;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '4':{
+            i = 1;
+            j = 0;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '5':{
+            i = 1;
+            j = 1;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '6':{
+            i = 1;
+            j = 2;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '7':{
+            i = 2;
+            j = 0;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '8':{
+            i = 2;
+            j = 1;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+        case '9':{
+            i = 2;
+            j = 2;
+            if(m[i][j] == 'X' || m[i][j] == 'O'){
+                k=1;
+                cout<<"La casilla esta ocupada, intentalo con otra que este libre"<<endl;
+                cout<<endl;
+
+            }
+            break;
+        }
+
+        
     }
+
+}while(k==1);
+
+m[i][j] = 'X';
+}
+
+void Intro_aleatorio(char m[3][3]){
+    int i,j,k;
+    
+    srand(time(NULL));
+    do{
+    i= rand() % 3;
+    j= rand() % 3;
+    k=0;
+
+    if(m[i][j] == 'X' || m[i][j] == 'O'){
+        k=1;
+    }
+
+    }while(k==1);
+
+
+    m[i][j] = 'O';
+
+
+}
+   
 
 
 
