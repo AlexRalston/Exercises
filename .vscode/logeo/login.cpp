b17198502662768c75311d448624b74b268761d5
Alejandro Acedo Ralston <aleaceral@alum.us.es>
2023-03-17 00:55:16 +0100

    Casi depurado

--- .vscode/logeo/login.cpp
+++ .vscode/logeo/login.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(){
 
     string usuario, contrasenia;
-    int intentos = 3;
+    int intentos = 4;
     bool acceso_concedido = false;
 
     ifstream archivo("tablaLog.txt");
@@ -17,7 +17,7 @@ int main(){
         cout<<"El archivo no esta abierto. Abriendo archivo..."<<endl;
         archivo.open("tablaLog.txt");
         if(!archivo.is_open()){
-        cout<<"Error al abrir el archivo"<<endl;
+        cout<<"Error al abrir el archivo."<<endl;
         return 1;
         }
 
@@ -54,37 +54,41 @@ int main(){
             string juego;
             do{
             
-            cout<<"¿Que desea jugar? (Cuatroenraya/Hundir la flota): ";
-            
+            cout<<"¿Que desea jugar? (Cuatroenraya/HundirLaFlota): ";
             cin>>juego;
-            }while(juego!="Cuatroenraya" && juego!="Hundir la flota");
+
+            cout<<endl;
+            cout<<endl;
+
+            }while(juego!="Cuatroenraya" && juego!="HundirLaFlota");
 
             if(juego  == "Cuatroenraya") {
 
                 system("cuatroenraya.exe"); //accede al prompt del sistema y ejecuta cuatroenraya
 
             }
-            else if(juego == "Hundir la flota"){
+            else if(juego == "HundirLaFlota"){
 
-                system("tocado.exe"); //accede al prompt del sistema y ejecuta hundir la flota
+                system("tocado.exe");
+                 //accede al prompt del sistema y ejecuta hundir la flota
 
             }
-            else{
+            else{//Un poco redundante pero bueno, por si los bugs...
 
-                cout<<"Juego no reconocido"<<endl;
+                cout<<"Juego no reconocido."<<endl;
             }
         }
         else{
             intentos--;
             if (intentos > 0){
-                cout<<"Acceso denegado. Intentos restantes: "<<intentos<<endl;
+                cout<<"Acceso denegado.Si no esta registrado,escriba si a continuacion. Intentos restantes: "<<intentos<<endl;
                 string respuesta;
                 cout<<"¿Desea registrarse en el sistema?(si/no):";
                 cin>>respuesta;
                 if(respuesta == "si"){
     cout<<"Ingrese el nombre del usuario que desea registrar:  ";
     cin>>usuario;
-    cout<<"Ingrese la contraseña que desea para su registro:";
+    cout<<"Ingrese la contrasenia que desea para su registro:";
     cin>>contrasenia;
 
     // Verificar si el usuario ya existe
@@ -95,8 +99,9 @@ int main(){
         string usuario_archivo = linea.substr(0,pos);
         if (usuario == usuario_archivo) {
             cout<<endl;
+            
+            cout << "Usuario existente, porfavor registrase con otro nombre de usuario y/o contrasenia." << endl;
             cout<<endl;
-            cout << "Usuario existente, porfavor registrase con otro nombre de usuario y/o contraseña." << endl;
             archivo_lectura.close();
             return 1;
         }
@@ -107,11 +112,11 @@ int main(){
     ofstream archivo_salida("tablaLog.txt",ios_base::app);
     if(archivo_salida.is_open()){
         archivo_salida<<usuario<<"-"<<contrasenia<<endl;
-        cout<<"Registro exitoso"<<endl;
+        cout<<"Registro exitoso."<<endl;
         archivo_salida.close();
     }
     else{
-        cout<<"Error al abrir el archivo"<<endl;
+        cout<<"Error al abrir el archivo."<<endl;
         return 1;
     }
 }
